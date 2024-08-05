94c5ac2b9577a718f73bad0e774329bade6e3335
subhin <subhintm2018@gmail.com>
2024-08-05 12:37:18 +0530

    new file added from develpment

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom"
 const Home = () => {
   const navigate=useNavigate()
   return (
-    <div>Home Page <br />
+    <div>Home Pagench modified in development branch <br />
       <button className="border p-1" onClick={()=>navigate('/login')}>Login</button>
     </div>
   )
